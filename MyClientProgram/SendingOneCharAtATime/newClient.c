9ba29e9abc894c10f9f2b2fd9a8b40904cf4fbde
RamenSayami <ramen.sayami@gmail.com>
2017-09-27 19:02:35 +0545

    newClient to build generic package

--- MyClientProgram/SendingOneCharAtATime/newClient.c
+++ MyClientProgram/SendingOneCharAtATime/newClient.c
@@ -14,8 +14,25 @@
 #define TERMINATE 53
 
 #define THIS_CLIENT 0 // Change number for different instances please!
-
 char packet[10];
+void buildPacket(int clientId, int seqNum, int comSignal, char msg){
+        // char packet[10];
+        bzero(packet, 10);
+        int i=0;
+        packet[i++] = clientId+48;
+        packet[i++] = ':';
+        while(seqNum%10){
+            int n = seqNum % 10;
+            packet[i++] = 48+n;
+            seqNum = seqNum/10;
+        }
+        packet[i++] = ':';
+        packet[i++] = comSignal;
+        packet[i++] = ':';
+        packet[i++] = msg;
+        packet[i++] = '\n';
+        fprintf(stderr,"%s\n", packet);
+}  
 
 void error(const char *msg)
 {
@@ -29,41 +46,6 @@ void transmitData(int sockfd, char *buffer){
         error("ERROR writing to socket");
         
 }
-void buildPacket(int clientId, int seqNum, int comSignal, char msg){
-        // char packet[10];
-        bzero(packet, 10);
-        int i=0;
-        packet[i++] = clientId;
-        packet[i++] = ':';
-        while(seqNum%10){
-            int n = seqNum % 10;
-            packet[i++] = 48+n;
-            seqNum = seqNum/10;
-        }
-        packet[i++] = ':';
-        packet[i++] = comSignal +48;
-        packet[i++] = ':';
-        packet[i++] = msg;
-        packet[i++] = '\n';
-        fprintf(stderr,"%s", packet);
-}  
-
-struct MessageDetails
-{
-    int seqNo;
-    char correctChar;
-    char recievedMessage1[10];
-    char recievedMessage2[10];
-    int commSignal;
-
-} messageArray[1000];
-char bufferRecv[10];
-
-char actualMessageDecoded[1000];
-
-
-// MessageDetails messageArray[1000];
-
 int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
@@ -98,56 +80,42 @@ int main(int argc, char *argv[])
 	buffer[i] = 'a';
     }
     fgets(buffer,10000,stdin);
+    int i =0;
     // Sliding window apply garnu parcha..
     // window size 5 jati..
     // dont send 6th character unless 1st is double acked
     // keep resending after some milisecond.
     // use struct? as a class? garo huncha hola ra?
 
-    int sendingCounter =0;
-    int flag = 1;
-    int i = 0;
-    while(flag){
-        // for(int i=0+sendingCounter; i<5+sendingCounter; i++){
-            // bzero(toSendBuffer, 10);
-            if(buffer[i] == '\n')
-                flag = -1;
-            else{
-                buildPacket(THIS_CLIENT, i, SEND, buffer[i]);
-                transmitData(sockfd, packet);    
-            }
-            
-        // }
-
-        // while(read(sockfd,bufferRecv,10000)){
-        //     if(bufferRecv[0] == THIS_CLIENT & bufferRecv[2]>48){
-        //         if(bufferRecv[4] == ACK1){
-        //             bzero(toSendBuffer, 10);
-        //                 toSendBuffer[0] = 48+THIS_CLIENT;
-        //                 toSendBuffer[1] = ":";
-        //                 toSendBuffer[2] = 48+bufferRecv[];
-        //                 toSendBuffer[3] = ':';
-        //                 toSendBuffer[4] = SEND;
-        //                 toSendBuffer[5] = ':';
-        //                 toSendBuffer[6] = buffer[i];
-        //                 toSendBuffer[7] = '\n';
-        //         }
-        //     }
-        // }
-            
-             
-            
-        // }
+    while(buffer[i] != '\n'){
+        buildPacket(THIS_CLIENT, i, SEND, buffer[i]);
+        // char toSendBuffer[10];
+        // bzero(toSendBuffer, 10);
+        // toSendBuffer[0] = 48+i;
+        // toSendBuffer[1] = ':';
+        // toSendBuffer[2] = SENDING;
+        // toSendBuffer[3] = ':';
+        // toSendBuffer[4] = buffer[i];
+        // toSendBuffer[5] = '\n';
+            transmitData(sockfd, packet);
+        i++;
     }
+        // char toSendBuffer[10];
+        // bzero(toSendBuffer, 10);
+        // toSendBuffer[0] = '\n';
+        //     transmitData(sockfd, toSendBuffer);
+
+    // n = write(sockfd,buffer,strlen(buffer));
+    // if (n < 0) 
+    //      error("ERROR writing to socket");
     while(1){
         bzero(buffer,10001);
         n = read(sockfd,buffer,10000);
+        if (n < 0) 
+            error("ERROR reading from socket");
         printf("%s",buffer);    
     }
     
     close(sockfd);
     return 0;
 }
-
-
-                
